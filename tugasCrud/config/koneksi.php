4db7e43b51059bba0b5b5696395e0c5045b2219d
suryaludyakananta9 <suryaludyakananta9@gmail.com>
2026-06-24 19:36:15 +0800

    Update koneksi.php

--- tugasCrud/config/koneksi.php
+++ tugasCrud/config/koneksi.php
@@ -1,6 +1,6 @@
 <?php
 // Menggunakan ekstensi mysqli bawaan dengan database db_akademik dan port 3307
-$koneksi = mysqli_connect("localhost", "root", "", "db_akademik", 3307);
+$koneksi = mysqli_connect("localhost", "root", "", "db_akademik", 3306);
 
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
