5d6bbcdc5df0f7de65c9eb14b5347af929280311
suryaludyakananta9 <suryaludyakananta9@gmail.com>
2026-06-24 19:46:11 +0800

    Update proses.php

--- tugasCrud/public/proses.php
+++ tugasCrud/public/proses.php
@@ -6,7 +6,6 @@ if (isset($_POST['update'])) {
     $id_nilai    = $_POST['id_nilai'];
     $nilai_huruf = $_POST['nilai_huruf'];
     
-    // Mengeksekusi pembaruan nilai_huruf berdasarkan id_nilai target
     $query = "UPDATE nilai SET nilai_huruf = '$nilai_huruf' WHERE id_nilai = '$id_nilai'";
               
     if (mysqli_query($koneksi, $query)) {
