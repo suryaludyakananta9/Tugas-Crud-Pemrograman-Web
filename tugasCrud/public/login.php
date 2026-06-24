2011fc9166ff2387b07caf68b33b35542b79afab
suryaludyakananta9 <suryaludyakananta9@gmail.com>
2026-06-24 19:49:06 +0800

    Update login.php

--- tugasCrud/public/login.php
+++ tugasCrud/public/login.php
@@ -7,11 +7,9 @@ if (isset($_POST['login'])) {
     $username = $_POST['username'];  
     $password = $_POST['password'];  
     
-    // Mengambil data user berdasarkan username
     $query = mysqli_query($koneksi, "SELECT * FROM users WHERE username='$username'");  
     $user = mysqli_fetch_assoc($query);  
     
-    // Verifikasi kecocokan username dan password
     if ($user && password_verify($password, $user['password'])) {  
         $_SESSION['login'] = true;  
         $_SESSION['username'] = $username;  
